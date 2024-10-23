1cf0dee02fbde86449dcd3fa85694911201a67c5
GinesMeca <ginesmeca11@hotmail.com>
2024-10-23 15:47:47 +0200

    Fixing bug coming from previous merge

--- skforecast/ForecasterAutoregDirect/ForecasterAutoregDirect.py
+++ skforecast/ForecasterAutoregDirect/ForecasterAutoregDirect.py
@@ -1299,7 +1299,7 @@ class ForecasterAutoregDirect(ForecasterBase):
         prediction_index = expand_index(
                                index = last_window_index,
                                steps = max(steps)
-                           )[np.array(steps) - 1]
+                           )[0][np.array(steps) - 1]
         if isinstance(last_window_index, pd.DatetimeIndex) and np.array_equal(
             steps, np.arange(min(steps), max(steps) + 1)
         ):
