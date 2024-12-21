33c0308a3c61c00a28a6982ef559233bc173f86a
JavierEscobarOrtiz <javier.escobar.ortiz@gmail.com>
2024-12-21 15:32:51 +0100

    update_index_warnings

--- skforecast/utils/tests/tests_utils/test_preprocess_exog.py
+++ skforecast/utils/tests/tests_utils/test_preprocess_exog.py
@@ -1,8 +1,10 @@
 # Unit test preprocess_exog
 # ==============================================================================
+import re
 import pytest
 import numpy as np
 import pandas as pd
+from skforecast.exceptions import IndexWarning
 from skforecast.utils import preprocess_exog
 
 
@@ -51,7 +53,15 @@ def test_output_preprocess_exog_when_exog_index_is_DatetimeIndex_but_has_not_fre
             data = np.arange(3),
             index = pd.to_datetime(["1990-01-01", "1990-01-02", "1990-01-03"])
         )
-    results = preprocess_exog(exog)
+    warn_msg = re.escape(
+        "`exog` has a pandas DatetimeIndex without a frequency. The index "
+        "will be replaced by a RangeIndex starting from 0 with a step of 1. "
+        "To avoid this warning, set the frequency of the DatetimeIndex using "
+        "`exog = exog.asfreq('desired_frequency', fill_value=np.nan)`."
+    )
+    with pytest.warns(IndexWarning, match=warn_msg):
+        results = preprocess_exog(exog)
+
     expected = (np.arange(3),
                 pd.RangeIndex(start=0, stop=3, step=1)
                )
@@ -65,7 +75,15 @@ def test_output_preprocess_exog_when_exog_index_is_not_DatetimeIndex_or_RangeInd
     Test values returned by when exog is a pandas Series without DatetimeIndex or RangeIndex.
     """
     exog = pd.Series(data=np.arange(3), index=['0', '1', '2'])
-    results = preprocess_exog(exog)
+    warn_msg = re.escape(
+        "`exog` has an unsupported index type (not pandas DatetimeIndex or "
+        "RangeIndex). The index will be replaced by a RangeIndex starting "
+        "from 0 with a step of 1. To avoid this warning, ensure that "
+        "`exog.index` is a DatetimeIndex with a frequency or a RangeIndex."
+    )
+    with pytest.warns(IndexWarning, match=warn_msg):
+        results = preprocess_exog(exog)
+
     expected = (np.arange(3),
                 pd.RangeIndex(start=0, stop=3, step=1)
                )
