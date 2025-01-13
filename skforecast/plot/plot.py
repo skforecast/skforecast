463c0052b28ab8d33d88a439655870f41191aa97
JavierEscobarOrtiz <javier.escobar.ortiz@gmail.com>
2025-01-13 15:55:22 +0100

    update_code_example

--- skforecast/plot/plot.py
+++ skforecast/plot/plot.py
@@ -339,15 +339,19 @@ def calculate_lag_autocorrelation(
 
     Examples
     --------
-    >>> from skforecast.plot import calculate_lag_autocorrelation
-    >>> import pandas as pd
-    >>> data = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    >>> calculate_lag_autocorrelation(data = data, n_lags = 4)
-       lag  partial_autocorrelation_abs  partial_autocorrelation  autocorrelation_abs  autocorrelation
-    0    1                     0.777778                 0.777778             0.700000         0.700000
-    1    4                     0.360707                -0.360707             0.078788        -0.078788
-    2    3                     0.274510                -0.274510             0.148485         0.148485
-    3    2                     0.227273                -0.227273             0.412121         0.412121
+    ```python
+    import pandas as pd
+    from skforecast.plot import calculate_lag_autocorrelation
+
+    data = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    calculate_lag_autocorrelation(data = data, n_lags = 4)
+    
+    #    lag  partial_autocorrelation_abs  partial_autocorrelation  autocorrelation_abs  autocorrelation
+    # 0    1                     0.777778                 0.777778             0.700000         0.700000
+    # 1    4                     0.360707                -0.360707             0.078788        -0.078788
+    # 2    3                     0.274510                -0.274510             0.148485         0.148485
+    # 3    2                     0.227273                -0.227273             0.412121         0.412121
+    ```
 
     """
 
