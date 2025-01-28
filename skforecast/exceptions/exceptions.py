5850fcffa6abee56f26e329623ec6c685133fbe4
Joaquin Amat <joraquin.amat@veeva.com>
2025-01-28 14:06:56 +0000

    testing rich format for warnings

--- skforecast/exceptions/exceptions.py
+++ skforecast/exceptions/exceptions.py
@@ -9,7 +9,11 @@
 The skforecast.exceptions module contains all the custom warnings and error 
 classes used across skforecast.
 """
-
+import warnings
+import textwrap
+from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
 
 class DataTypeWarning(UserWarning):
     """
@@ -22,10 +26,10 @@ class DataTypeWarning(UserWarning):
 
     def __str__(self):
         extra_message = (
-            "\n You can suppress this warning using: "
+            "You can suppress this warning using: "
             "warnings.simplefilter('ignore', category=DataTypeWarning)"
         )
-        return self.message + " " + extra_message
+        return self.message + "\n" + extra_message
 
 
 class DataTransformationWarning(UserWarning):
@@ -37,10 +41,10 @@ class DataTransformationWarning(UserWarning):
 
     def __str__(self):
         extra_message = (
-            "\n You can suppress this warning using: "
+            "You can suppress this warning using: "
             "warnings.simplefilter('ignore', category=DataTransformationWarning)"
         )
-        return self.message + " " + extra_message
+        return self.message + "\n" + extra_message
 
 
 class IgnoredArgumentWarning(UserWarning):
@@ -53,10 +57,10 @@ class IgnoredArgumentWarning(UserWarning):
 
     def __str__(self):
         extra_message = (
-            "\n You can suppress this warning using: "
+            "You can suppress this warning using: "
             "warnings.simplefilter('ignore', category=IgnoredArgumentWarning)"
         )
-        return self.message + " " + extra_message
+        return self.message + "\n" + extra_message
 
 
 class IndexWarning(UserWarning):
@@ -69,10 +73,10 @@ class IndexWarning(UserWarning):
 
     def __str__(self):
         extra_message = (
-            "\n You can suppress this warning using: "
+            "You can suppress this warning using: "
             "warnings.simplefilter('ignore', category=IndexWarning)"
         )
-        return self.message + " " + extra_message
+        return self.message + "\n" + extra_message
 
 
 class LongTrainingWarning(UserWarning):
@@ -85,10 +89,10 @@ class LongTrainingWarning(UserWarning):
 
     def __str__(self):
         extra_message = (
-            "\n You can suppress this warning using: "
+            "You can suppress this warning using: "
             "warnings.simplefilter('ignore', category=LongTrainingWarning)"
         )
-        return self.message + " " + extra_message
+        return self.message + "\n" + extra_message
 
 
 class MissingExogWarning(UserWarning):
@@ -102,10 +106,10 @@ class MissingExogWarning(UserWarning):
 
     def __str__(self):
         extra_message = (
-            "\n You can suppress this warning using: "
+            "You can suppress this warning using: "
             "warnings.simplefilter('ignore', category=MissingExogWarning)"
         )
-        return self.message + " " + extra_message
+        return self.message + "\n" + extra_message
 
 
 class MissingValuesWarning(UserWarning):
@@ -120,10 +124,10 @@ class MissingValuesWarning(UserWarning):
 
     def __str__(self):
         extra_message = (
-            "\n You can suppress this warning using: "
+            "You can suppress this warning using: "
             "warnings.simplefilter('ignore', category=MissingValuesWarning)"
         )
-        return self.message + " " + extra_message
+        return self.message + "\n" + extra_message
 
 
 class OneStepAheadValidationWarning(UserWarning):
@@ -135,10 +139,10 @@ class OneStepAheadValidationWarning(UserWarning):
 
     def __str__(self):
         extra_message = (
-            "\n You can suppress this warning using: "
+            "You can suppress this warning using: "
             "warnings.simplefilter('ignore', category=OneStepAheadValidationWarning)"
         )
-        return self.message + " " + extra_message
+        return self.message + "\n" + extra_message
 
 
 class ResidualsUsageWarning(UserWarning):
@@ -151,10 +155,10 @@ class ResidualsUsageWarning(UserWarning):
 
     def __str__(self):
         extra_message = (
-            "\n You can suppress this warning using: "
+            "You can suppress this warning using: "
             "warnings.simplefilter('ignore', category=ResidualsUsageWarning)"
         )
-        return self.message + " " + extra_message
+        return self.message + "\n" + extra_message
 
 
 class UnknownLevelWarning(UserWarning):
@@ -167,10 +171,10 @@ class UnknownLevelWarning(UserWarning):
 
     def __str__(self):
         extra_message = (
-            "\n You can suppress this warning using: "
+            "You can suppress this warning using: "
             "warnings.simplefilter('ignore', category=UnknownLevelWarning)"
         )
-        return self.message + " " + extra_message
+        return self.message + "\n" + extra_message
 
 
 class SaveLoadSkforecastWarning(UserWarning):
@@ -183,10 +187,10 @@ class SaveLoadSkforecastWarning(UserWarning):
 
     def __str__(self):
         extra_message = (
-            "\n You can suppress this warning using: "
+            "You can suppress this warning using: "
             "warnings.simplefilter('ignore', category=SaveLoadSkforecastWarning)"
         )
-        return self.message + " " + extra_message
+        return self.message + "\n" + extra_message
 
 
 class SkforecastVersionWarning(UserWarning):
@@ -199,10 +203,10 @@ class SkforecastVersionWarning(UserWarning):
 
     def __str__(self):
         extra_message = (
-            "\n You can suppress this warning using: "
+            "You can suppress this warning using: "
             "warnings.simplefilter('ignore', category=SkforecastVersionWarning)"
         )
-        return self.message + " " + extra_message
+        return self.message + "\n" + extra_message
 
 
 warn_skforecast_categories = [
@@ -219,3 +223,80 @@ warn_skforecast_categories = [
     SaveLoadSkforecastWarning,
     SkforecastVersionWarning,    
 ]
+
+def format_warning_handler(message, category, filename, lineno, file=None, line=None):
+    """
+    Custom warning handler to format warnings in a box for skforecast custom
+    warnings.
+
+    Parameters
+    ----------
+    message : str
+        Warning message.
+    category : str
+        Warning category.
+    filename : str
+        Filename where the warning was raised.
+    lineno : int
+        Line number where the warning was raised.
+    file : file
+        File where the warning was raised.
+    line : str
+        Line where the warning was raised.
+
+    Returns
+    -------
+    None
+
+    """
+
+    if isinstance(message, tuple(warn_skforecast_categories)):
+        width = 88
+        title = type(message).__name__
+        output_text = ["\n"]
+
+        wrapped_message = textwrap.fill(str(message), width=width - 2, expand_tabs=True, replace_whitespace=True)
+        title_top_border = f"╭{'─' * ((width - len(title) - 2) // 2)} {title} {'─' * ((width - len(title) - 2) // 2)}╮"
+        bottom_border = f"╰{'─' * width}╯"
+        output_text.append(title_top_border)
+
+        for line in wrapped_message.split('\n'):
+            output_text.append(f"│ {line.ljust(width - 2)} │")
+
+        output_text.append(bottom_border)
+        output_text = "\n".join(output_text)
+        color = '\033[38;5;208m'
+        reset = '\033[0m'
+        output_text = f"{color}{output_text}{reset}"
+        print(output_text)
+    else:
+        # Fallback to default Python warning formatting
+        warnings._original_showwarning(message, category, filename, lineno, file, line)
+
+
+def rich_warning_handler(message, category, filename, lineno, file=None, line=None):
+    """
+    Custom handler for warnings that uses rich to display formatted panels.
+    """
+    if isinstance(message, tuple(warn_skforecast_categories)):
+        console = Console()
+        panel = Panel(
+            Text(
+                f"{message}\n\nCategory: {category.__name__}\nLocation: {filename}:{lineno}",
+                justify="left",
+            ),
+            title=category.__name__,
+            title_align="center",
+            border_style="color(214)",
+            width=88,
+        )
+        console.print(panel)
+    else:
+        # Fallback to default Python warning formatting
+        warnings._original_showwarning(message, category, filename, lineno, file, line)
+
+
+if not hasattr(warnings, "_original_showwarning"):
+    warnings._original_showwarning = warnings.showwarning
+
+warnings.showwarning = rich_warning_handler
