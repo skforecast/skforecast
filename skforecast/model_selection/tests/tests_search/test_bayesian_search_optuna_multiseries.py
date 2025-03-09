942b24154bfc27d47bd3017d3d1941d0feb7694f
JavierEscobarOrtiz <javier.escobar.ortiz@gmail.com>
2025-03-09 20:16:04 +0100

    skforecast/model_selection/tests/tests_search/test_bayesian_search_optuna_multiseries.py

--- skforecast/model_selection/tests/tests_search/test_bayesian_search_optuna_multiseries.py
+++ skforecast/model_selection/tests/tests_search/test_bayesian_search_optuna_multiseries.py
@@ -198,6 +198,7 @@ def test_ValueError_bayesian_search_optuna_multiseries_when_search_space_names_d
 # pytest -m "not slow" --verbose
 @pytest.mark.slow
 @pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python3.8 or higher")
+@pytest.mark.skipif(sys.platform == "darwin", reason="Fails in MacOS")
 def test_results_output_bayesian_search_optuna_multiseries_with_mocked_when_lags_grid_dict():
     """
     Test output of _bayesian_search_optuna_multiseries in ForecasterRecursiveMultiSeries 
@@ -775,6 +776,7 @@ def test_results_output_bayesian_search_optuna_multiseries_with_kwargs_create_st
     pd.testing.assert_frame_equal(results, expected_results)
 
 
+@pytest.mark.skipif(sys.platform == "darwin", reason="Fails in MacOS")
 def test_results_output_bayesian_search_optuna_multiseries_with_kwargs_study_optimize():
     """
     Test output of _bayesian_search_optuna_multiseries in ForecasterRecursiveMultiSeries 
