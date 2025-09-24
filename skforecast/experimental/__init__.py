f8bbc829167cbfc875c60742919ae9da5f488051
Joaquín Amat Rodrigo <JoaquinAmatRodrigo@users.noreply.github.com>
2025-09-24 16:56:29 +0200

    ongoing

--- skforecast/experimental/__init__.py
+++ skforecast/experimental/__init__.py
@@ -1,3 +1,4 @@
 from ._experimental import calculate_distance_from_holiday
 from ._forecaster_stats import ForecasterStats
 from ._validation import backtesting_stats
+from ._search import grid_search_stats, random_search_stats
