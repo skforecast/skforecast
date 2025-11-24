9881b682e68e3d93a89104eb684c985fbd5a0baf
JavierEscobarOrtiz <javier.escobar.ortiz@gmail.com>
2025-11-24 16:17:13 +0100

    update_feature_importances_method

--- skforecast/recursive/tests/tests_forecaster_recursive_classifier/test_get_feature_importances.py
+++ skforecast/recursive/tests/tests_forecaster_recursive_classifier/test_get_feature_importances.py
@@ -36,6 +36,24 @@ def test_NotFittedError_is_raised_when_forecaster_is_not_fitted():
         forecaster.get_feature_importances()
 
 
+def test_output_and_UserWarning_get_feature_importances_when_CalibratedClassifierCV_no_calibrated_classifiers_():
+    """
+    Test output of get_feature_importances when estimator is CalibratedClassifierCV.
+    and calibrated_classifiers_ is empty.
+    """
+    forecaster = ForecasterRecursiveClassifier(
+        CalibratedClassifierCV(LogisticRegression()), lags=3
+    )
+    forecaster.is_fitted = True  # To skip the check for fitted forecaster
+
+    warn_msg = re.escape(
+        "The CalibratedClassifierCV instance is not fitted or does not "
+        "expose 'calibrated_classifiers_'. Unable to retrieve importances."
+    )
+    with pytest.warns(UserWarning, match = warn_msg):
+        assert forecaster.get_feature_importances() is None
+
+
 def test_output_and_UserWarning_get_feature_importances_when_regressor_no_attributes():
     """
     Test output of get_feature_importances when estimator is HistGradientBoostingClassifier.
@@ -48,7 +66,6 @@ def test_output_and_UserWarning_get_feature_importances_when_regressor_no_attrib
     forecaster.fit(y=y)
 
     estimator = forecaster.regressor
-    expected = None
 
     warn_msg = re.escape(
         f"Impossible to access feature importances for estimator of type "
@@ -57,8 +74,7 @@ def test_output_and_UserWarning_get_feature_importances_when_regressor_no_attrib
         f"attribute `feature_importances_` or `coef_`."
     )
     with pytest.warns(UserWarning, match = warn_msg):
-        results = forecaster.get_feature_importances()
-        assert results is expected
+        assert forecaster.get_feature_importances() is None
 
 
 def test_output_get_feature_importances_when_regressor_is_RandomForestClassifier():
@@ -147,7 +163,7 @@ def test_output_get_feature_importances_with_exog():
     pd.testing.assert_frame_equal(results, expected)
 
 
-def test_output_get_feature_importances_when_CalibratedClassifierCV():
+def test_output_get_feature_importances_when_CalibratedClassifierCV_LogisticRegression():
     """
     Test output of get_feature_importances when using CalibratedClassifierCV with
     LogisticRegression as base estimator.
@@ -159,12 +175,101 @@ def test_output_get_feature_importances_when_CalibratedClassifierCV():
     forecaster.fit(y=y)
 
     results = forecaster.get_feature_importances(sort_importance=False)
-    expected = pd.DataFrame({
-                   'feature': ['lag_1', 'lag_2', 'lag_3'],
-                   'importance': np.array([0.166667, 0.166667, 0.166667])
-               })
+    expected = pd.DataFrame(
+        {
+            "cv_fold": [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4],
+            "classes": [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3],
+            "lag_1": [
+                0.17392860358936565,
+                -0.3184535617446897,
+                0.14452495815532343,
+                0.5352421566187048,
+                -0.5173562179196608,
+                -0.017885938699045225,
+                0.03377500382837249,
+                -0.26741874378694175,
+                0.23364373995857027,
+                0.17065538525910254,
+                -0.007321092653028191,
+                -0.16333429260607474,
+                0.2726424414624969,
+                -0.3334117120536584,
+                0.06076927059116098,
+            ],
+            "lag_2": [
+                -0.01611048290713891,
+                0.11077280342639155,
+                -0.09466232051925359,
+                0.2280201941787555,
+                -0.25569262632673506,
+                0.027672432147979882,
+                0.10076280520266548,
+                -0.003800599666816256,
+                -0.09696220553585018,
+                0.04863953728034529,
+                -0.04348611843417003,
+                -0.005153418846175248,
+                -0.04023988196420971,
+                0.04327266081780304,
+                -0.003032778853593432,
+            ],
+            "lag_3": [
+                -0.3653103558512643,
+                -0.1654339349554506,
+                0.530744290806715,
+                -0.1725577743251356,
+                0.061039588331654,
+                0.11151818599348112,
+                -0.2058169676549178,
+                -0.2336644022888757,
+                0.43948136994379405,
+                -0.16896234946742456,
+                -0.11116568424885509,
+                0.2801280337162793,
+                -0.25566300391265306,
+                -0.21052789543738035,
+                0.46619089935003255,
+            ],
+        }
+    )
 
     pd.testing.assert_frame_equal(results, expected)
+
+
+def test_output_get_feature_importances_when_CalibratedClassifierCV_LGBMClassifier():
+    """
+    Test output of get_feature_importances when using CalibratedClassifierCV with
+    LGBMClassifier as base estimator, min_child_samples=5 due to small sample size.
+    """
+    forecaster = ForecasterRecursiveClassifier(
+                     regressor = CalibratedClassifierCV(LGBMClassifier(
+                         min_child_samples=5, verbose=-1, random_state=123
+                     ), cv=3),
+                     lags      = 3
+                 )
+    forecaster.fit(y=y)
+
+    results = forecaster.get_feature_importances(sort_importance=True)
+    expected = pd.DataFrame(
+        {
+            "cv_fold": [0, 0, 0, 1, 1, 1, 2, 2, 2],
+            "feature": [
+                "lag_3",
+                "lag_1",
+                "lag_2",
+                "lag_1",
+                "lag_2",
+                "lag_3",
+                "lag_1",
+                "lag_3",
+                "lag_2",
+            ],
+            "importance": [387, 376, 324, 368, 366, 338, 386, 361, 338],
+        }
+    ).astype({"importance": int})
+    expected.index = pd.Index([2, 0, 1, 3, 4, 5, 6, 8, 7], dtype=int)
+
+    pd.testing.assert_frame_equal(results.astype({"importance": int}), expected)
 
 
 def test_output_get_feature_importances_when_window_features():
