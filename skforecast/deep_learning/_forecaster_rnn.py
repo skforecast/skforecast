5ca2b4279a1275309e6a3b3fde7d9453ba5d3892
Javier Escobar Ortiz <javier.escobar.ortiz@gmail.com>
2026-04-14 11:39:31 +0200

    feat: Enhance device selection for PyTorch backend in ForecasterRnn

--- skforecast/deep_learning/_forecaster_rnn.py
+++ skforecast/deep_learning/_forecaster_rnn.py
@@ -1039,7 +1039,12 @@ class ForecasterRnn(ForecasterBase):
         if self.keras_backend_ == "torch":
             
             import torch
-            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            if torch.cuda.is_available():
+                device = torch.device("cuda")
+            elif torch.backends.mps.is_available():
+                device = torch.device("mps")
+            else:
+                device = torch.device("cpu")
             print(f"Using '{self.keras_backend_}' backend with device: {device}")
 
             torch_device = torch.device(device)
