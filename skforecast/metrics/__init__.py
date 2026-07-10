531b8e43d8e17f96acf7030d45e3d1ffe4265fd6
kalichiruvelli-art <kalichiruvelli@gmail.com>
2026-07-09 20:16:40 -0400

    feat(metrics): export winkler_score and weighted_interval_score from __init__

--- skforecast/metrics/__init__.py
+++ skforecast/metrics/__init__.py
@@ -3,9 +3,25 @@ from .metrics import (
     root_mean_squared_scaled_error,
     symmetric_mean_absolute_percentage_error,
     add_y_train_argument,
+    _any_metric_needs_y_train,
     crps_from_predictions,
     crps_from_quantiles,
     calculate_coverage,
     create_mean_pinball_loss,
+    winkler_score,
+    weighted_interval_score,
     _get_metric,
 )
+
+__all__ = [
+    "mean_absolute_scaled_error",
+    "root_mean_squared_scaled_error",
+    "symmetric_mean_absolute_percentage_error",
+    "add_y_train_argument",
+    "crps_from_predictions",
+    "crps_from_quantiles",
+    "calculate_coverage",
+    "create_mean_pinball_loss",
+    "winkler_score",
+    "weighted_interval_score",
+]
